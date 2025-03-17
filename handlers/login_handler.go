aec54ed509450b9625985fa0910f4e57755bc85e
Ruben <rbenbal43@gmail.com>
2025-03-17 11:27:12 -0600

    la inteligencia me persigue

--- handlers/login_handler.go
+++ handlers/login_handler.go
@@ -1,44 +1,51 @@
 package handlers
 
 import (
-	"errors"
+	//"errors"
 	"fmt"
 	"net/http"
-	"sena-crud/utils"
-	"time"
+	"sena-crud/db"
+	//"sena-crud/utils"
+	//"time"
 )
 
-type Login struct {
-	HashedPassword string
-	SessionToken   string
-	CSRFToken      string
+type Usuario struct {
+	IdUser   int
+	username string
+	password string
 }
 
-var users = map[string]Login{}
+//type Login struct {
+//	HashedPassword string
+//	SessionToken   string
+//	CSRFToken      string
+//}
 
-var AuthError = errors.New("No autorizado")
+//var users = map[string]Login{}
 
-func authorize(r *http.Request) error {
-	username := r.FormValue("username")
-	user, ok := users[username]
+//var AuthError = errors.New("No autorizado")
 
-	if !ok {
-		return AuthError
-	}
-
-	// obtener el token de las cookies
-	st, err := r.Cookie("session_token")
-	if err != nil || st.Value == "" || st.Value != user.SessionToken {
-		return AuthError
-	}
-
-	csrf := r.Header.Get("X-CSRF-Token")
-	if csrf != user.CSRFToken || csrf == "" {
-		return AuthError
-	}
-
-	return nil
-}
+//func authorize(r *http.Request) error {
+//	username := r.FormValue("username")
+//	user, ok := users[username]
+//
+//	if !ok {
+//		return AuthError
+//	}
+//
+//	// obtener el token de las cookies
+//	st, err := r.Cookie("session_token")
+//	if err != nil || st.Value == "" || st.Value != user.SessionToken {
+//		return AuthError
+//	}
+//
+//	csrf := r.Header.Get("X-CSRF-Token")
+//	if csrf != user.CSRFToken || csrf == "" {
+//		return AuthError
+//	}
+//
+//	return nil
+//}
 
 func Home(w http.ResponseWriter, r *http.Request) {
 	//fmt.Fprint(w, "Home")
@@ -60,102 +67,116 @@ func Register(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		if _, ok := users[username]; ok {
-			// en go podemos obtener dos valores de un map valor, ok : users["clave"]. El segundo valor es un booleano
-			// en este caso el guión bajo indica que ignoramos el valor
-			er := http.StatusConflict
-			http.Error(w, "El usuario ya existe", er)
+		var count int
+
+		conexion := db.ConexionDB()
+		err := conexion.QueryRow("SELECT COUNT(*) FROM usuarios WHERE username=?", username).Scan(&count)
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		if count > 0 {
+			fmt.Fprintf(w, "El usuario ya existe")
 			return
 		}
 
-		hashedPassword, _ := utils.HashPassword(password)
-		users[username] = Login{
-			HashedPassword: hashedPassword,
+		registrar, err := conexion.Prepare("INSERT INTO usuarios(username, contraseña) VALUES(?, ?)")
+
+		if err != nil {
+			panic(err.Error())
 		}
+
+		registrar.Exec(username, password)
+
+		//hashedPassword, _ := utils.HashPassword(password)
+		//users[username] = Login{
+		//	HashedPassword: hashedPassword,
+		//}
 
 		//fmt.Fprintln(w, "User registered succesfully!")
 		http.Redirect(w, r, "/lista-clientes", 301)
 	}
 }
 
-func LoginFunc(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "POST" {
-		username := r.FormValue("username")
-		password := r.FormValue("password")
-
-		user, ok := users[username]
-		if !ok || utils.CheckPasswordHash(password, user.HashedPassword) {
-			er := http.StatusBadRequest
-			http.Error(w, "Usuario o contraseña incorrecto", er)
-			return
-		}
-
-		sessionToken := utils.GenerateToken(32)
-		csrfToken := utils.GenerateToken(32)
-
-		http.SetCookie(w, &http.Cookie{
-			Name:     "session_token",
-			Value:    sessionToken,
-			Expires:  time.Now().Add(24 * time.Hour),
-			HttpOnly: true,
-		})
-
-		http.SetCookie(w, &http.Cookie{
-			Name:     "csrf_token",
-			Value:    csrfToken,
-			Expires:  time.Now().Add(24 * time.Hour),
-			HttpOnly: false,
-		})
-
-		// guardar el token en la base de datos
-		user.SessionToken = sessionToken
-		user.CSRFToken = csrfToken
-		users[username] = user
-
-		http.Redirect(w, r, "/lista-clientes", 301)
-	}
-}
-
-func Protected(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "POST" {
-		if err := authorize(r); err != nil {
-			er := http.StatusUnauthorized
-			http.Error(w, "No autorizado", er)
-			return
-		}
-
-		username := r.FormValue("username")
-		fmt.Fprintf(w, "Validación exitosa! bienvenido, %s", username)
-	}
-}
-
-func Logout(w http.ResponseWriter, r *http.Request) {
-	if err := authorize(r); err != nil {
-		er := http.StatusUnauthorized
-		http.Error(w, "No autorizado", er)
-		return
-	}
-
-	http.SetCookie(w, &http.Cookie{
-		Name:     "session_token",
-		Value:    "",
-		Expires:  time.Now().Add(-time.Hour),
-		HttpOnly: true,
-	})
-
-	http.SetCookie(w, &http.Cookie{
-		Name:     "csrf_token",
-		Value:    "",
-		Expires:  time.Now().Add(-time.Hour),
-		HttpOnly: false,
-	})
-
-	//borrar los tokens de la base de datos
-	username := r.FormValue("username")
-	user, _ := users[username]
-	user.SessionToken = ""
-	user.CSRFToken = ""
-	users[username] = user
-
-	fmt.Fprintln(w, "logout exitoso!")
-}
+//func LoginFunc(w http.ResponseWriter, r *http.Request) {
+//	if r.Method == "POST" {
+//		username := r.FormValue("username")
+//		password := r.FormValue("password")
+//
+//		user, ok := users[username]
+//		if !ok || utils.CheckPasswordHash(password, user.HashedPassword) {
+//			er := http.StatusBadRequest
+//			http.Error(w, "Usuario o contraseña incorrecto", er)
+//			return
+//		}
+//
+//		sessionToken := utils.GenerateToken(32)
+//		csrfToken := utils.GenerateToken(32)
+//
+//		http.SetCookie(w, &http.Cookie{
+//			Name:     "session_token",
+//			Value:    sessionToken,
+//			Expires:  time.Now().Add(24 * time.Hour),
+//			HttpOnly: true,
+//		})
+//
+//		http.SetCookie(w, &http.Cookie{
+//			Name:     "csrf_token",
+//			Value:    csrfToken,
+//			Expires:  time.Now().Add(24 * time.Hour),
+//			HttpOnly: false,
+//		})
+//
+//		// guardar el token en la base de datos
+//		user.SessionToken = sessionToken
+//		user.CSRFToken = csrfToken
+//		users[username] = user
+//
+//		http.Redirect(w, r, "/lista-clientes", 301)
+//	}
+//}
+//
+////func Protected(w http.ResponseWriter, r *http.Request) {
+////	if r.Method == "POST" {
+////		if err := authorize(r); err != nil {
+////			er := http.StatusUnauthorized
+////			http.Error(w, "No autorizado", er)
+////			return
+////		}
+////
+////		username := r.FormValue("username")
+////		fmt.Fprintf(w, "Validación exitosa! bienvenido, %s", username)
+////	}
+////}
+//
+//func Logout(w http.ResponseWriter, r *http.Request) {
+//	if err := authorize(r); err != nil {
+//		er := http.StatusUnauthorized
+//		http.Error(w, "No autorizado", er)
+//		return
+//	}
+//
+//	http.SetCookie(w, &http.Cookie{
+//		Name:     "session_token",
+//		Value:    "",
+//		Expires:  time.Now().Add(-time.Hour),
+//		HttpOnly: true,
+//	})
+//
+//	http.SetCookie(w, &http.Cookie{
+//		Name:     "csrf_token",
+//		Value:    "",
+//		Expires:  time.Now().Add(-time.Hour),
+//		HttpOnly: false,
+//	})
+//
+//	//borrar los tokens de la base de datos
+//	username := r.FormValue("username")
+//	user, _ := users[username]
+//	user.SessionToken = ""
+//	user.CSRFToken = ""
+//	users[username] = user
+//
+//	fmt.Fprintln(w, "logout exitoso!")
+//}
--- utils/utils.go
+++ utils/utils.go
@@ -1,27 +1,27 @@
 package utils
 
-import (
-	"crypto/rand"
-	"encoding/base64"
-	"golang.org/x/crypto/bcrypt"
-	"log"
-)
-
-func HashPassword(password string) (string, error) {
-	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 10)
-	return string(bytes), err
-}
-
-func CheckPasswordHash(password, hash string) bool {
-	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
-	return err == nil
-}
-
-func GenerateToken(length int) string {
-	bytes := make([]byte, length)
-	if _, err := rand.Read(bytes); err != nil {
-		log.Fatalf("Falló al generar token: %v", err)
-	}
-
-	return base64.URLEncoding.EncodeToString(bytes)
-}
+//import (
+//	"crypto/rand"
+//	"encoding/base64"
+//	"golang.org/x/crypto/bcrypt"
+//	"log"
+//)
+//
+//func HashPassword(password string) (string, error) {
+//	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 10)
+//	return string(bytes), err
+//}
+//
+//func CheckPasswordHash(password, hash string) bool {
+//	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+//	return err == nil
+//}
+//
+//func GenerateToken(length int) string {
+//	bytes := make([]byte, length)
+//	if _, err := rand.Read(bytes); err != nil {
+//		log.Fatalf("Falló al generar token: %v", err)
+//	}
+//
+//	return base64.URLEncoding.EncodeToString(bytes)
+//}
