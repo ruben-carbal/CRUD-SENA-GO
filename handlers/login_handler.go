1bdc009045cf39ebd5a6e556176176f4b67855b1
Ruben <rbenbal43@gmail.com>
2025-03-17 08:08:42 -0600

    intento de login

--- handlers/login_handler.go
+++ handlers/login_handler.go
@@ -1,10 +1,11 @@
 package handlers
 
 import (
-	//"fmt"
+	"errors"
+	"fmt"
 	"net/http"
 	"sena-crud/utils"
-	//"time"
+	"time"
 )
 
 type Login struct {
@@ -14,6 +15,30 @@ type Login struct {
 }
 
 var users = map[string]Login{}
+
+var AuthError = errors.New("No autorizado")
+
+func authorize(r *http.Request) error {
+	username := r.FormValue("username")
+	user, ok := users[username]
+
+	if !ok {
+		return AuthError
+	}
+
+	// obtener el token de las cookies
+	st, err := r.Cookie("session_token")
+	if err != nil || st.Value == "" || st.Value != user.SessionToken {
+		return AuthError
+	}
+
+	csrf := r.Header.Get("X-CSRF-Token")
+	if csrf != user.CSRFToken || csrf == "" {
+		return AuthError
+	}
+
+	return nil
+}
 
 func Home(w http.ResponseWriter, r *http.Request) {
 	//fmt.Fprint(w, "Home")
@@ -53,20 +78,84 @@ func Register(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func LoginFunc() (w http.ResponseWriter, r *http.Request) {
+func LoginFunc(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
 		username := r.FormValue("username")
 		password := r.FormValue("password")
 
 		user, ok := users[username]
-		if !ok {
+		if !ok || utils.CheckPasswordHash(password, user.HashedPassword) {
 			er := http.StatusBadRequest
 			http.Error(w, "Usuario o contraseña incorrecto", er)
 			return
 		}
+
+		sessionToken := utils.GenerateToken(32)
+		csrfToken := utils.GenerateToken(32)
+
+		http.SetCookie(w, &http.Cookie{
+			Name:     "session_token",
+			Value:    sessionToken,
+			Expires:  time.Now().Add(24 * time.Hour),
+			HttpOnly: true,
+		})
+
+		http.SetCookie(w, &http.Cookie{
+			Name:     "csrf_token",
+			Value:    csrfToken,
+			Expires:  time.Now().Add(24 * time.Hour),
+			HttpOnly: false,
+		})
+
+		// guardar el token en la base de datos
+		user.SessionToken = sessionToken
+		user.CSRFToken = csrfToken
+		users[username] = user
+
+		http.Redirect(w, r, "/lista-clientes", 301)
 	}
 }
 
-//func Logout() (w http.ResponseWriter, r *http.Request) {}
-//
-//func Protected() (w http.ResponseWriter, r *http.Request) {}
+func Protected(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		if err := authorize(r); err != nil {
+			er := http.StatusUnauthorized
+			http.Error(w, "No autorizado", er)
+			return
+		}
+
+		username := r.FormValue("username")
+		fmt.Fprintf(w, "Validación exitosa! bienvenido, %s", username)
+	}
+}
+
+func Logout(w http.ResponseWriter, r *http.Request) {
+	if err := authorize(r); err != nil {
+		er := http.StatusUnauthorized
+		http.Error(w, "No autorizado", er)
+		return
+	}
+
+	http.SetCookie(w, &http.Cookie{
+		Name:     "session_token",
+		Value:    "",
+		Expires:  time.Now().Add(-time.Hour),
+		HttpOnly: true,
+	})
+
+	http.SetCookie(w, &http.Cookie{
+		Name:     "csrf_token",
+		Value:    "",
+		Expires:  time.Now().Add(-time.Hour),
+		HttpOnly: false,
+	})
+
+	//borrar los tokens de la base de datos
+	username := r.FormValue("username")
+	user, _ := users[username]
+	user.SessionToken = ""
+	user.CSRFToken = ""
+	users[username] = user
+
+	fmt.Fprintln(w, "logout exitoso!")
+}
